50efdb81bfbdd75778c1873efcde4dddcd248833
fengyongkang <wp06894@1.cn>
2023-06-29 16:44:33 +0800

    笔记 第三章完结

--- src/org/fenixsoft/jvm/chapter3/TestSerialGCAllocation.java
+++ src/org/fenixsoft/jvm/chapter3/TestSerialGCAllocation.java
@@ -10,18 +10,24 @@ public class TestSerialGCAllocation {
 
     /**
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * 参数限制了Java堆大小为20MB不可扩展，其中10MB分配给新生代，剩下的10MB分配给老年代。
      */
     public static void testAllocation() {
         byte[] allocation1, allocation2, allocation3, allocation4;
         allocation1 = new byte[2 * _1MB];
         allocation2 = new byte[2 * _1MB];
         allocation3 = new byte[2 * _1MB];
-        allocation4 = new byte[4 * _1MB];  // 出现一次Minor GC
+        allocation4 = new byte[4 * _1MB];  // 出现一次Minor GC，垃圾收集期间虚拟机发现已有三个2MB大小的对象并且全部无法放入Survivor空间，只好放入通过分配担保机制提前转移到老年代中。
     }
 
     /**
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
      * -XX:PretenureSizeThreshold=3145728
+     * <p>
+     * PretenureSizeThreshold参数只对Serial和ParNew两款新生代收集器有效.
+     * 查看当前虚拟机用的什么收集器先执行 jpd -l查看所有虚拟机的进程id。
+     * 然后执行 jmap -heap 对应的id，会显示当前的垃圾收集器和当前的堆使用情况。
+     * -XX:+UseSerialGC 设置虚拟机使用Serial+Serial Old收集器组合进行内存回收，但是我设置后查看收集器名称是"Mark Sweep Compact GC"
      */
     public static void testPretenureSizeThreshold() {
         byte[] allocation;
@@ -73,6 +79,9 @@ public class TestSerialGCAllocation {
         allocation7 = new byte[2 * _1MB];
     }
 
+    public static void main(String[] args) {
+        testTenuringThreshold();
+    }
 
 
 }
